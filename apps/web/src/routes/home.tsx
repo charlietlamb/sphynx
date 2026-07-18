ba2a065367deb2a2535d194fac572629c1af42b1
Charlie Lamb <charlielamb20@icloud.com>
2026-07-18 18:06:52 +0100

    feat: landing page at /home

---
+++ apps/web/src/routes/home.tsx
@@ -0,0 +1,6 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { Landing } from "@/components/landing/landing";
+
+export const Route = createFileRoute("/home")({
+  component: Landing,
+});
